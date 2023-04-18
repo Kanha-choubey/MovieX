aef993a06c60eeca92a5bdb66bf9834c1253ce9d
Kanha Choubey <kanhaarmyoo7@gmail.com>
2023-04-18 23:20:51 +0530

    components and Redux setup

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,13 +1,12 @@
 import axios from "axios";
-const Base_URL = "https://api.themoviedb.org/3";
-const TMDB_TOKEN = import.meta.env.VITE_APP_TEDB_TOKEN;
-
-const headers = () => {
-  Authentication: "bearer " + Base_URL;
+const BASE_URL = "https://api.themoviedb.org/3";
+const TMDB_TOKEN = import.meta.env.VITE_APP_TMDB_TOKEN;
+const headers = {
+  Authorization: "bearer " + TMDB_TOKEN,
 };
 export const fetchDataFromApi = async (url, params) => {
   try {
-    const { data } = await axios.get(Base_URL + url, {
+    const { data } = await axios.get(BASE_URL + url, {
       headers,
       params,
     });
